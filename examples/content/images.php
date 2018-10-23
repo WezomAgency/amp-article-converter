8f5b153d773e0d93bcff0b61c9015eda8b9d864a
Дутченко Олег <dutchenko.o.wezom@gmail.com>
2018-10-23 10:31:38 +0300

    core

---
+++ examples/content/images.php
@@ -0,0 +1,24 @@
+<h2>Images</h2>
+
+<p>
+	<img src="https://c1.staticflickr.com/6/5081/5252291555_830a56721b_b.jpg"
+			srcset="https://c1.staticflickr.com/6/5081/5252291555_830a56721b_b.jpg 2x"
+			border="2"
+			align="center"
+			width="1024"
+			height="680"
+			usemap="#nav"
+			sizes="(max-width: 40em) 5vw, 10vw"
+			vspace="60">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus hic quo quod sed voluptas. Atque delectus, doloremque est eveniet ex hic illo inventore magnam numquam, odit omnis ratione sed veritatis!
+</p>
+<p>
+<img src="https://c1.staticflickr.com/6/5081/5252291555_830a56721b_b.jpg"
+		border="2"
+		align="center" ismap
+		width="1024"
+		hspace="20"
+		sizes="(max-width: 30em) 100vw, (max-width: 50em) 50vw"
+		longdesc="dzen.txt"
+		height="680">
+</p>
